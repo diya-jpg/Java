9f90664e174922b46d46e98dbd0b2dd46c4fe063
Diya Bari <diyabari2004@gmail.com>
2024-10-29 14:23:58 +0530

    commit

---
+++ queue_interleave.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class queue_interleave {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer>st=new Stack<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        int x=q.size()/2;
+        for(int i=1;i<=x;i++)
+        {
+st.push(q.remove());
+        }
+      while(!st.isEmpty()){
+q.add(st.pop());
+      }
+      for(int i=1;i<=x;i++){
+       st.push(q.remove());
+      }
+      for(int i=1;i<=x;i++){
+        q.add(st.pop());
+        q.add(q.remove());
+      }
+      while(q.size()>0){
+        st.push(q.remove());
+      }
+      while(!st.isEmpty()){
+        q.add(st.pop());
+      }
+      System.out.println(q);
+    }
+}
