1796c6089c261f4b3f589e35bdcbfebbecf29be2
Diya Bari <diyabari2004@gmail.com>
2024-11-11 22:48:42 +0530

    commit

---
+++ Recursion_sorted.java
@@ -0,0 +1,12 @@
+public class Recursion_sorted {
+    static boolean sorted(int []arr,int index){
+        if(index==arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1]&&sorted(arr,index+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,5,16,8};
+        System.out.println(sorted(arr,0));
+    }
+}
--- recursion_count.java
+++ recursion_count.java
@@ -1,19 +1,19 @@
 public class recursion_count {
-  public static int rec(int n) {
-      if (n == 0) {
-          return 0;
+  public static int rec(int n,int count) {
+     
+      if(n==0){
+        return count;
       }
-      
-      // If the last digit is 0, add 1 to the count
-      if (n % 10 == 0) {
-          return 1 + rec(n / 10);
-      } else {
-          return rec(n / 10);
+      if(n%10==0){
+        return rec(n/10,count+1);
+      }
+      else{
+        return rec(n/10,count);
       }
   }
   
   public static void main(String[] args) {
       int n = 30201;
-      System.out.println(rec(n)); // Output should be the count of 0s in n
+      System.out.println(rec(n,0)); 
   }
 }
