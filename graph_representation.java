fe7929509d8bfd1fc85c7bea2354042a6b2317ff
Diya Bari <diyabari2004@gmail.com>
2025-01-16 21:44:04 +0530

    commit

--- graph_representation.java
+++ graph_representation.java
@@ -20,7 +20,19 @@ public class graph_representation {
             }
         }
     }
-
+    public void addEdgesinMatrixwithweight(int edges[][], boolean isDirected) {
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+    int w=edge[2];
+            if (isDirected) {
+                adjMatrix[u][v] = w;
+            } else {
+                adjMatrix[u][v] = w;
+                adjMatrix[v][u] = w;
+            }
+        }
+    }
     
     void printMatrix() {
         for (int i = 0; i < adjMatrix.length; i++) {
@@ -33,17 +45,22 @@ public class graph_representation {
 
  
     public static void main(String[] args) {
-        int edges[][] = { { 0, 2 }, { 0, 1 }, { 1, 3 } }; 
+        int edges[][] = { { 0, 2,10 }, { 0, 1,20 }, { 1, 3,30 } }; 
         int nodes = 4;
 
        
         graph_representation graph = new graph_representation(nodes);
 
      
-        graph.addEdgesinMatrix(edges, false);
+        graph.addEdgesinMatrix(edges, true);
 
       
         System.out.println("Adjacency Matrix:");
         graph.printMatrix();
+        System.out.println();
+        graph_representation graph2 = new graph_representation(nodes);
+
+        graph2.addEdgesinMatrixwithweight(edges, true);
+        graph2.printMatrix();
     }
 }
