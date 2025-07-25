2a0e9c335e34cb6ac6a30da92985ced32c3da082
diya-jpg <diyabari2004@gmail.com>
2025-07-25 07:50:50 +0530

    commit

--- pattern_butterfly.java
+++ pattern_butterfly.java
@@ -4,21 +4,6 @@ public class pattern_butterfly {
             for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
-          for(int k=i;k<5;k++){
-            System.out.print(" ");
-          }
-           for(int k=i;k<5;k++){
-            System.out.print(" ");
-          }
-          for(int j=0;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-           for(int i = 5-1; i >=0; i--){
-             for(int j=0;j<=i;j++){
-                System.out.print("*");
-            }
           for(int k=i;k<5-1;k++){
             System.out.print(" ");
           }
@@ -30,5 +15,21 @@ public class pattern_butterfly {
             }
             System.out.println();
         }
+           for(int i =0; i <5; i++){
+             for(int j=5-1;j>i;j--){
+                System.out.print("*");
+            }
+          for(int k=0;k<=i;k++){
+            System.out.print(" ");
+          }
+           for(int k=0;k<=i;k++){
+            System.out.print(" ");
+          }
+          for(int j=5-1;j>i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        
+          }
     }
 }
